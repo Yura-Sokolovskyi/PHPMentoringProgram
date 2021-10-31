e4acd931fba745248c0cb76898760f53b550a1ac
Yurii_Sokolovskyi <sokolovskyi.dev@gmail.com>
2021-10-31 14:07:54 +0200

    "Tree : Top View" task

---
+++ test/DataStructuresAndAlgorithms/TreeTopView/TreeTopViewBuilderTest.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\DataStructuresAndAlgorithms\TreeBuilder;
+use App\DataStructuresAndAlgorithms\TreeTopView\TreeTopViewBuilder;
+use PHPUnit\Framework\TestCase;
+
+class TreeTopViewBuilderTest extends TestCase
+{
+    public function testTreeTopView()
+    {
+        $tree = (new TreeBuilder())->buildFromArray([1, 2, 5, 3, 6, 4]);
+
+        $this->assertEquals('1 2 5 6', (new TreeTopViewBuilder())->build($tree));
+    }
+}
